5c348fb3637f891387f327dc0197640aa0f7c9f6
RyoMiyata <miyata028@gmail.com>
2025-08-24 10:29:04 +0900

    Initial Commit

---
+++ MatchingClient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchingClient
+//
+//  Created by 宮田涼 on 2025/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchingClient/MatchingClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingClientApp.swift
+//  MatchingClient
+//
+//  Created by 宮田涼 on 2025/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchingClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
